27426d89c3011c947824d03e8cecce2d2a7efb4d
tofudriver <007kyg2@naver.com>
2024-04-05 14:57:19 +0900

    simple plan of the makeQuestion

--- src/main/java/com/geomhwein/go/controller/UserController.java
+++ src/main/java/com/geomhwein/go/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.geomhwein.go.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
@@ -47,5 +49,12 @@ public class UserController {
 	public String homeworkList() {
 		return "user/homeworkList";
 	}
-	
+	@GetMapping("/makeQuestion")
+	public String makeQuestion(@RequestParam("username")String username,Model model){
+		model.addAttribute("username",username);
+		return "user/makeQuestion";
+	}
+
+
+
 }
