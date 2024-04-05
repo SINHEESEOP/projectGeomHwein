5b4ac02323592ea86da9f1f35fad64c875d9f3d9
tofudriver <007kyg2@naver.com>
2024-04-05 16:08:46 +0900

    freeeeeeeeeeeeeeeeeee

--- src/main/java/com/geomhwein/go/command/questionVO.java
+++ src/main/java/com/geomhwein/go/command/questionVO.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class makeAnswerVO {
+public class questionVO {
 	
 	private String userId;
 	private String ansCn;
--- src/main/java/com/geomhwein/go/controller/UserController.java
+++ src/main/java/com/geomhwein/go/controller/UserController.java
@@ -5,6 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 @RequestMapping("/user")
@@ -54,6 +57,13 @@ public class UserController {
 		model.addAttribute("username",username);
 		return "user/makeQuestion";
 	}
+	@PostMapping("/registQuestionForm")
+	public String registQuestionForm(@RequestParam("userId")String userId,@RequestParam("questionDate")String questionDate,@RequestParam("questionCn")String questionCn) {
+		
+		
+		return "";
+	}
+	
 
 
 
