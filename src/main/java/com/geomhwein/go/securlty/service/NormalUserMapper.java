99a4ae5e8a0b0f7b745143843efdef09d4d5231b
SINHEESEOP <shfurwnd97@gmail.com>
2024-04-22 15:01:32 +0900

    결제 페이지

--- src/main/java/com/geomhwein/go/securlty/service/NormalUserMapper.java
+++ src/main/java/com/geomhwein/go/securlty/service/NormalUserMapper.java
@@ -1,5 +1,6 @@
 package com.geomhwein.go.securlty.service;
 
+import com.geomhwein.go.command.CartVO;
 import com.geomhwein.go.command.EducationGroupVO;
 import com.geomhwein.go.command.UserAuthVO;
 import com.geomhwein.go.util.Criteria;
@@ -15,5 +16,6 @@ public interface NormalUserMapper {
 	public ArrayList<EducationGroupVO> getList(Criteria cri);
 	public ArrayList<UserAuthVO> getUserList(Criteria cri, String id);
 	public ArrayList<EducationGroupVO> getList2(Criteria cri);
-	public ArrayList<EducationGroupVO> getCart(String userId);
+	public ArrayList<CartVO> getCart(String userId);
+	public int deleteCart(Long groupNo, String userId);
 }
